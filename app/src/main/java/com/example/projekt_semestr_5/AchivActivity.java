4c34edfb5c842c0abb59bccbd864c1e1ffd78167
AdrianMarkiewicz <amarkiewicz27@gmail.com>
2020-01-19 16:05:56 +0100

    Achievement Activity

--- app/src/main/java/com/example/projekt_semestr_5/AchivActivity.java
+++ app/src/main/java/com/example/projekt_semestr_5/AchivActivity.java
@@ -1,4 +1,32 @@
 package com.example.projekt_semestr_5;
 
-public class AchivActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AchivActivity extends AppCompatActivity {
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.achievement);
+        frameLayout = (FrameLayout) findViewById(R.id.pierwszeKroki);
+        frameLayout.setAlpha(1);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                // if(button.getVisibility()==View.INVISIBLE)
+
+            }
+        });
+
+    }
+
 }
